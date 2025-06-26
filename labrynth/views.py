b4e619446feda15a37448ad8efc681e40e803e13
Nicole Zhang <nicolezhang@macbookair.home>
2025-06-26 11:58:04 -0400

    Added more functions and tried more with the icons

--- labrynth/views.py
+++ labrynth/views.py
@@ -5,3 +5,13 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'labrynth/home.html')
+
+def keep_walking(request):
+    return render(request, 'labrynth/keep_walking.html')
+
+# login function for later if I figure out how forms work...
+def signup(request):
+    return render(request, 'labrynth/signup.html', {})
+
+def login(request)
+    return render(request, 'labrynth/login.html', {})
